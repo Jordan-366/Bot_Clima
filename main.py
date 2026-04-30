6b5b742951bf85f9b922b105ba83b06eea67f702
Jordan-366 <jordan.pinheiro2005@gmail.com>
2026-04-29 21:50:51 -0300

    funcionando localmente e espero que no actions tbm

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import smtplib
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 my_email = os.getenv("EMAIL_JORD")
 my_password = os.getenv("EMAIL_PASSWORD")
 api = os.getenv("API_CLIMA")
@@ -21,16 +23,18 @@ reponse_jund = requests.get(OWM_Endpoint, params=weather_params)
 weather_jundiai = reponse_jund.json()
 rain_jund = False
 
+print(weather_jundiai)
+
 for hour in weather_jundiai["list"]:
     if hour["weather"][0]["id"] < 700:
         rain_jund = True
 
 if rain_jund:
-    connection = smtplib.SMTP("smtp.gmail.com")
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email, to_addrs="", msg='Vai chover hoje, se liga!!')
-    connection.close()
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email, msg='Subject:Aleta de chuva!\n\nVai chover hoje, se liga!!')
+        connection.close()
 
 
 
