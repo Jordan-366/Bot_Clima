e6a5015a176694058bc6568273396c3f67f90f71
Jordan-366 <jordan.pinheiro2005@gmail.com>
2026-04-29 21:01:37 -0300

    testando2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import smtplib
 
-my_email = "jordan.pinheiro2005@gmail.com"
+my_email = ""
 
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "5bacae889c8f9f3f8379f6204760b13d"
+api_key = ""
 
 weather_params = {
     "lat": -23.187080,
@@ -27,7 +27,7 @@ if rain_jund:
     connection = smtplib.SMTP("smtp.gmail.com")
     connection.starttls()
     connection.login(user=my_email, password='')
-    connection.sendmail(from_addr=my_email, to_addrs="jordan.pinheiro2005@gmail.com", msg='Vai chover hoje, se liga!!')
+    connection.sendmail(from_addr=my_email, to_addrs="", msg='Vai chover hoje, se liga!!')
     connection.close()
 
 
