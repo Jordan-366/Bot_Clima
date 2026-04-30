80353302a0fc0011dbd374419090e8fd062e4982
Jordan-366 <jordan.pinheiro2005@gmail.com>
2026-04-29 21:22:58 -0300

    teoricamente funcionando

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import requests
 import smtplib
+import os
 
-my_email = ""
+my_email = os.getenv("EMAIL_JORD")
+my_password = os.getenv("EMAIL_PASSWORD")
+api = os.getenv("API_CLIMA")
 
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = ""
 
 weather_params = {
     "lat": -23.187080,
     "lon": -46.884048,
     "cnt": 4,
-    "appid": api_key,
+    "appid": api,
 }
 
 reponse_jund = requests.get(OWM_Endpoint, params=weather_params)
@@ -26,7 +28,7 @@ for hour in weather_jundiai["list"]:
 if rain_jund:
     connection = smtplib.SMTP("smtp.gmail.com")
     connection.starttls()
-    connection.login(user=my_email, password='')
+    connection.login(user=my_email, password=my_password)
     connection.sendmail(from_addr=my_email, to_addrs="", msg='Vai chover hoje, se liga!!')
     connection.close()
 
